cf740c2496491dcdd86ccbbadde257e3701b544c
Frengky <frengky@node8.net>
2018-05-08 10:16:26 +0700

    use container and minor change

--- basic_code/rest_api/app/Http/Controllers/UserReviewController.php
+++ basic_code/rest_api/app/Http/Controllers/UserReviewController.php
@@ -7,46 +7,82 @@ use App\UserReview;
 
 class UserReviewController extends Controller
 {
+    protected $reviews;
+
+    public function __construct(UserReview $reviews)
+    {
+        $this->reviews = $reviews;
+    }
+
     public function index()
     {
-        return UserReview::all();
+        return $reviews = $this->reviews::all();
     }
 
     /**
-     * POST
-     * order_id=2
-     * &product_id=1
-     * &user_id=5
-     * &rating=3
-     * &review=mantap%20beuds
+     * @method POST
+     * @link user-reviews/
+     * @param
+     *  order_id
+     *  product_id
+     *  user_id
+     *  rating
+     *  review
+     * @return data
      */
     public function store(Request $request)
-    {
-        return UserReview::create($request->all());
+    {   
+        $data =  $this->reviews::create($request->all());
+        if ($data) {
+            return response()
+            ->json(['message' => 'success created',
+                    'data' => $data], 200);
+        }
+        return response()
+            ->json(['message' => 'something wrong in the server'], 400);
     }
 
+    /**
+     * @method POST
+     * @link user-reviews/{id}
+     * @param
+     *  order_id
+     *  product_id
+     *  user_id
+     *  rating
+     *  review
+     * @return data
+     */
     public function update(Request $request, $id)
     {
-        $review = UserReview::findOrFail($id);
+        $review = $this->reviews::findOrFail($id);
         $review->update($request->all());
-        // if($review){
-        //     $review->order_id = $request->order_id;
-        //     $review->product_id = $request->product_id;
-        //     $review->user_id = $request->user_id;
-        //     $review->rating = $request->rating;
-        //     $review->review = $request->review;
-        //     $page->save();
-        // }
 
-        return $review;
+        if ($review) {
+            return response()
+            ->json(['message' => 'success updated',
+                    'data' => $review], 200);
+        }
+        return response()
+            ->json(['message' => 'something wrong in the server'], 400);
     }
 
+    /**
+     * @method DELETE
+     * @link user-reviews/{id}
+     * @return data
+     */
     public function delete(Request $request, $id)
     {
-        $review = UserReview::findOrFail($id);
+        $review = $this->reviews::findOrFail($id);
         $review->delete();
         
+        if ($review) {
+            return response()
+            ->json(['message' => 'success deleted',
+                    'data' => $review], 200);
+        }
         return response()
-            ->json(['message' => 'Deleted!']);
+            ->json(['message' => 'something wrong in the server'], 400);
     }
 }
